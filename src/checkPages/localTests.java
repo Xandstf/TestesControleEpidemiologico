c74d213c3a61f7db54e0ed7f74cdff000a57c6c3
Emanoela Masqueti Simão <50922306+manumsimao@users.noreply.github.com>
2021-06-27 18:41:30 -0300

    added input tests

--- src/checkPages/localTests.java
+++ src/checkPages/localTests.java
@@ -2,31 +2,43 @@ package checkPages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.List;
 
 public class localTests {
 
 	public void runTests(WebDriver driver) {
 		System.out.println("\n\n## ## | Iniciando testes de Local | ## ##");
 		
-		System.out.print("\n # Verificando abertura da página ");
+		System.out.print("\n # Verificando abertura da pagina ");
 		openPageHome(driver);
 		
-		System.out.print("\n # Abrir adição de local ");
+		System.out.print("\n # Abrir adicao de local ");
 		openCadastro(driver);
 		
-		System.out.print("\n # Abrir alteração de local ");
+		System.out.print("\n # Abrir alteracao de local ");
 		openAlterar(driver);
 		
-		System.out.print("\n # Abrir exclusão de local ");
+		System.out.print("\n # Abrir exclusao de local ");
 		openExcluir(driver);
 		
 		System.out.print("\n # Abrir listagem de um local ");
-		openPraga(driver);
+		openLocal(driver);
 		
 		System.out.print("\n # Abrir listagem de locais ");
-		openPragas(driver);
+		openLocais(driver);
+
+		System.out.print("\n # Checando se os campos de Cadastro de Locais estao habilitados ");
+		checkCamposHabilitados(driver);
+
+		System.out.println("\n # Checando os campos de tipo number ");
+		checkNumberInput(driver);
+
 	}
-	
+
 	public void openPageHome(WebDriver driver) {
 		driver.get("http://localhost/PaginaControleEpidemiologico/local/dashLocal.html");
 		if(driver.findElements(By.xpath("//*[contains(text(),'DASHBOARD LOCAL')]")).size() > 0) {
@@ -69,7 +81,7 @@ public class localTests {
 		
 	}
 	
-	public void openPraga(WebDriver driver) {
+	public void openLocal(WebDriver driver) {
 		driver.get("http://localhost/PaginaControleEpidemiologico/local/dashLocal.html");
 		driver.findElement(By.partialLinkText("LISTAR UM LOCAL")).click();
 		if(driver.findElements(By.xpath("//*[contains(text(),'Selecione o local que gostaria de ver:')]")).size() > 0) {
@@ -80,15 +92,50 @@ public class localTests {
 		
 	}
 	
-	public void openPragas(WebDriver driver) {
+	public void openLocais(WebDriver driver) {
 		driver.get("http://localhost/PaginaControleEpidemiologico/local/dashLocal.html");
 		driver.findElement(By.partialLinkText("LISTAR LOCAIS")).click();
-		if(driver.findElements(By.xpath("//*[contains(text(),'Características')]")).size() > 0) {
+		if(driver.findElements(By.xpath("//*[contains(text(),'Raio')]")).size() > 0) {
 			System.out.print("[PASS]");
 		}else {
 			System.out.print("[REPROVED]");
 		}
-		
 	}
-	
+
+	public void checkCamposHabilitados(WebDriver driver) {
+		driver.get("http://localhost/PaginaControleEpidemiologico/local/cadastroLocal.html");
+		String[] names = {"cep_ponto_central", "raio", "populacao", "caracteristicas"};
+		for (String name: names) {
+			System.out.println("\nNome do campo: " + name);
+			if(driver.findElement(By.name(name)).isEnabled()) {
+				System.out.print("[PASS]");
+			} else {
+				System.out.print("[REPROVED]");
+			}
+		}
+	}
+
+	private void checkNumberInput(WebDriver driver) {
+		driver.get("http://localhost/PaginaControleEpidemiologico/local/cadastroLocal.html");
+		String[] names = {"cep_ponto_central", "raio", "populacao"};
+		for (String name: names) {
+			WebElement element = driver.findElement(By.name(name));
+			System.out.println("\nNome do campo: " + name);
+			System.out.println("\n Nao permite texto: ");
+			element.sendKeys("teste");
+			if(element.getAttribute("value").equals("teste")) {
+				System.out.print("[REPROVED]");
+			} else {
+				System.out.print("[PASS]");
+			}
+			element.clear();
+			System.out.println("\n Permite numeros: ");
+			element.sendKeys("123");
+			if(element.getAttribute("value").equals("123")) {
+				System.out.print("[PASS]");
+			} else {
+				System.out.print("[REPROVED]");
+			}
+		}
+	}
 }
--- src/checkPages/pragaTests.java
+++ src/checkPages/pragaTests.java
@@ -2,6 +2,7 @@ package checkPages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class pragaTests {
 
@@ -25,8 +26,15 @@ public class pragaTests {
 		
 		System.out.print("\n # Abrir listagem de pragas ");
 		openPragas(driver);
+
+		System.out.print("\n # Checando se os campos de Cadastro de Pragas estao habilitados ");
+		checkCamposHabilitados(driver);
+
+		System.out.println("\n # Checando o campo de tipo number ");
+		checkNumberInput(driver);
 	}
-	
+
+
 	public void openPageHome(WebDriver driver) {
 		driver.get("http://localhost/PaginaControleEpidemiologico/praga/dashPraga.html");
 		if(driver.findElements(By.xpath("//*[contains(text(),'DASHBOARD PRAGA')]")).size() > 0) {
@@ -90,6 +98,39 @@ public class pragaTests {
 		}
 		
 	}
-	
+	public void checkCamposHabilitados(WebDriver driver) {
+		driver.get("http://localhost/PaginaControleEpidemiologico/local/cadastroPraga.html");
+		String[] names = {"codigo", "nome", "doencas_transmitidas", "tempo_vida", "modos_combate"};
+		for (String name: names) {
+			System.out.println("\nNome do campo: " + name);
+			if(driver.findElement(By.name(name)).isEnabled()) {
+				System.out.print("[PASS]");
+			} else {
+				System.out.print("[REPROVED]");
+			}
+		}
+	}
+
+	private void checkNumberInput(WebDriver driver) {
+		driver.get("http://localhost/PaginaControleEpidemiologico/local/cadastroPraga.html");
+
+			WebElement element = driver.findElement(By.name("codigo"));
+			System.out.println("\nNome do campo: codigo");
+			System.out.println("\n Nao permite texto: ");
+			element.sendKeys("teste");
+			if(element.getAttribute("value").equals("teste")) {
+				System.out.print("[REPROVED]");
+			} else {
+				System.out.print("[PASS]");
+			}
+			element.clear();
+			System.out.println("\n Permite numeros: ");
+			element.sendKeys("123");
+			if(element.getAttribute("value").equals("123")) {
+				System.out.print("[PASS]");
+			} else {
+				System.out.print("[REPROVED]");
+			}
+		}
 	
 }
